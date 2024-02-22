ee27006a3166dded834e3fc0d00f7b78ca222e66
Amogh CR <68552461+TheAmg@users.noreply.github.com>
2024-02-22 13:41:06 +0530

    local conf

--- src/main/java/com/spring/example/RestApplication.java
+++ src/main/java/com/spring/example/RestApplication.java
@@ -1,11 +1,13 @@
 package com.spring.example;
 
+import io.unlogged.Unlogged;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestApplication {
 
+	@Unlogged
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
